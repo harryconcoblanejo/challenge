92ba76d0cb7185f50e0e8594577813a6b5bb5bd5
Your Name <you@example.com>
2022-08-04 13:49:44 -0300

    challenge first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from "react";
 
-function App() {
+function App(): JSX.Element {
+  const [data, setData] = useState("");
+
+  const getData = () => {
+    console.log("saludando");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <button onClick={getData}></button>
+    </Fragment>
   );
 }
 
